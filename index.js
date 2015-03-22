0ec72944045e7658045578637b6ae00a4ee60bf2
Sveinn Fannar Kristjánsson <sveinnfannar@gmail.com>
2015-03-22 22:17:27 +0000

    Fixture references and basic reference resolving

--- index.js
+++ index.js
@@ -1,50 +1,109 @@
 'use strict';
 
-// Example state defenition
-var state = {
-  // Basic data generation
-  user: generateUser(),
-  client: generateClient(),
-  // Data references
-  accessToken: generateAccessToken({
-    userId: reference('user.id'),
-    clientId: reference('client.id')
-  }),
-  // Nested data
-  channels: {
-    published: generateChannel({ name: 'Published Channel', published: true }),
-    unpublished: generateChannel({ name: 'Un-published Channel', published: false })
-  },
-  // Generate multiple objects
-  collections: generateCollections(2, {
-    channelId: [reference('channels.published.id'), reference('channels.unpublished.id')]
-  }),
-  videos: generateVideos(4, {
-    channelId: [reference('channels.*.id')],
-    collectionId: [null, reference('channels.*.collections.*.id')]
-  }),
-  moments: generateMoments(6, {
-    channelId: [reference('channels.*.id')],
-    videoId: [null, reference('channels.*.videos.*.id')],
-    userId: reference('user.id')
-  }),
-  follows: [
-    generateFollow({ channelId: reference('channels.published.id'), userId: reference('user.id') }),
-    generateFollow({ channelId: reference('channels.unpublished.id'), userId: reference('user.id') })
-  ]
-};
+const _ = require('lodash');
+const assert = require('assert');
+const expect = require('chai').expect;
 
-// Helpers
-function reference () { }
-function resolve () { }
-function persist () {}
+// Create reference
+function reference (path) {
+  return {
+    _type: 'reference', path
+  };
+}
+
+// Resolve all references in a given state
+function resolve (state) {
+  return _.transform(state, function (resultState, value, key) {
+    resultState[key] = processItem(value, resultState);
+    return resultState;
+  }, {});
+
+  function processItem (item, resultState) { // TODO: Find a better function name
+    assert(item && resultState, 'item and resultState should be provided');
+    return _.mapValues(item, function (value) { 
+      if (isReference(value)) {
+        return resolveReference(value, resultState);
+      } else if (_.isObject(value)) {
+        return processItem(value, resultState);
+      } else {
+        return value;
+      }
+    });
+  }
+}
+
+function resolveReference (reference, state) {
+  console.log('Resolving reference with path: "%s" with state %j', reference.path, state);
+  return eval('state.' + reference.path); // TODO: Extract value at path without eval
+}
+
+function isReference(item) {
+  return _.isObject(item) && item._type === 'reference';
+}
 
 // Generators
-function generateUser () { }
-function generateClient () { }
-function generateAccessToken () { }
-function generateChannel () { }
-function generateVideos () { }
-function generateCollections () { }
-function generateMoments () { }
-function generateFollow () { }
+function generateUser (properties) {
+  return _.merge({
+    _type: 'user',
+    id: '?',
+    username: 'Swen'
+  }, properties);
+}
+
+function generateClient (properties) {
+  return _.merge({
+    _type: 'client',
+    id: '?'
+  }, properties);
+}
+
+function generateAccessToken (properties) {
+  return _.merge({
+    _type: 'accessToken',
+    id: 'a1',
+    token: 'abc'
+  }, properties);
+}
+
+// Example state defenition
+describe('', function () {
+  it('returns the same object if there are no references to resolve', function () {
+    const state = {
+      user: generateUser(),
+      client: generateClient(),
+    };
+
+    expect(resolve(state)).to.deep.equal(state);
+  });
+
+  it('resolves references for values defined before the reference', function () {
+    const state = {
+      user: generateUser({ id: 'u1' }),
+      client: generateClient({ id: 'c1' }),
+      accessToken: generateAccessToken({
+        userId: reference('user.id'),
+        clientId: reference('client.id')
+      })
+    };
+
+    const resolvedState = resolve(state);
+    expect(resolvedState.accessToken.userId).to.equal('u1');
+    expect(resolvedState.accessToken.clientId).to.equal('c1');
+  });
+
+  it('resolves nested references for values defined before the reference', function () {
+    const state = {
+      user: generateUser({ id: 'u1' }),
+      task: {
+        _type: 'task',
+        task: 'Buy milk',
+        info: {
+          userId: reference('user.id')
+        }
+      }
+    };
+
+    const resolvedState = resolve(state);
+    expect(resolvedState.task.info.userId).to.equal('u1');
+  });
+});
